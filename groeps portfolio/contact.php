52035f4a01898a6395cbe281327404ca3865506e
sem janssen <524273@vistacollege.nl>
2025-11-17 14:38:04 +0100

    Add files via upload

---
+++ groeps portfolio/contact.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact us</title>
+    <link rel="stylesheet" href="mainpage.css">
+</head>
+<body>
+
+<div class="menu-container">
+    <div class="balk"></div>
+
+    <div class="groepsnaam"><h1>L.M.M.S.</h1></div>
+    <div class="functie"><h1>Developers</h1></div>
+
+    <a href="mainpage.php" class="homepage-btn">Homepage</a>
+    <a href="projects.php" class="projects-btn">Projects</a>
+    <a href="team.php" class="team-btn">Our team</a>
+    <a href="contact.php" class="contact-btn">Contact us</a>
+</div>
+
+
+    
+</body>
+</html>
